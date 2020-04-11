a46965b754baab39eb2cb241e8b0e868033d451d
DanielaBeltranSaavedra <dpbeltran97@gmail.com>
2020-04-11 15:42:55 -0500

    Added my project

---
+++ app/src/main/java/com/raywenderlich/android/bottomsup/ui/feed/BeersActivity.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.bottomsup.ui.feed
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.GridLayoutManager
+import android.view.View
+import com.raywenderlich.android.bottomsup.R
+import com.raywenderlich.android.bottomsup.common.getViewModel
+import com.raywenderlich.android.bottomsup.common.subscribe
+import com.raywenderlich.android.bottomsup.ui.feed.adapter.BeersAdapter
+import com.raywenderlich.android.bottomsup.viewmodel.BeersViewModel
+import kotlinx.android.synthetic.main.activity_beers.*
+
+
+class BeersActivity : AppCompatActivity() {
+  private val viewModel by lazy { getViewModel<BeersViewModel>() }
+  private val adapter = BeersAdapter()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_beers)
+
+    initializeUi()
+
+    viewModel.errorData.subscribe(this, this::setErrorVisibility)
+    viewModel.loadingData.subscribe(this, this::showLoading)
+    viewModel.pageData.subscribe(this, adapter::clearIfNeeded)
+    viewModel.beerData.subscribe(this, adapter::addItems)
+
+
+    viewModel.getBeers() // request the data for the first time
+  }
+
+  private fun initializeUi() {
+    beersList.layoutManager = GridLayoutManager(this, 2)
+    beersList.itemAnimator = DefaultItemAnimator()
+    beersList.adapter = adapter
+    pullToRefresh.setOnRefreshListener(viewModel::onRefresh)
+  }
+
+  private fun showLoading(isLoading: Boolean) {
+    pullToRefresh.isRefreshing = isLoading
+  }
+
+  private fun setErrorVisibility(shouldShow: Boolean) {
+    errorView.visibility = if (shouldShow) View.VISIBLE else View.GONE
+    beersList.visibility = if (!shouldShow) View.VISIBLE else View.GONE
+  }
+
+
+}
---
+++ app/src/main/java/com/raywenderlich/android/bottomsup/viewmodel/BeersViewModel.kt
@@ -0,0 +1,49 @@
+package com.raywenderlich.android.bottomsup.viewmodel
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.raywenderlich.android.bottomsup.App
+import com.raywenderlich.android.bottomsup.model.Beer
+import com.raywenderlich.android.bottomsup.model.BeerResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class BeersViewModel : ViewModel() {
+    private val interactor by lazy { App.component.breweryInteractor() }
+    val errorData = MutableLiveData<Boolean>()
+    val loadingData = MutableLiveData<Boolean>()
+    val pageData = MutableLiveData<Int>()
+    val beerData = MutableLiveData<List<Beer>>()
+    fun getBeers() {
+        interactor.getBeers(pageData.value ?: 1, beersCallback())
+    }
+
+    fun onRefresh() {
+        pageData.value=1
+        getBeers()
+    }
+
+    private fun beersCallback() = object : Callback<BeerResponse> {
+        override fun onFailure(call: Call<BeerResponse>?, t: Throwable?) {
+            loadingData.value = false
+            errorData.value = true
+        }
+
+
+        override fun onResponse(call: Call<BeerResponse>?, response: Response<BeerResponse>?) {
+            loadingData.value = false //data has come, stop the loading
+            errorData.value = false
+
+            response?.body()?.run {
+                updateData(this)
+            }
+        }
+        private fun updateData(data: BeerResponse) {
+            pageData.value = data.currentPage + 1 // increment the page
+            beerData.value = data.beers
+        }
+
+
+    }
+}
